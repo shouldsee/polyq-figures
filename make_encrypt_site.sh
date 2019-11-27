1ecc960dec608a957add41ffe7db9164f88295a9
shouldsee <shouldsee.gem@gmail.com>
2019-11-27 18:16:51 +0000

    Create make_encrypt_site.sh

---
+++ make_encrypt_site.sh
@@ -0,0 +1,8 @@
+### password is reviewpaw
+DIR=${1:-$PWD}
+HASH=a93194bed5709a448522d69b9a722c02d1e7e71f
+find $DIR -type l -delete 
+mkdir -p $HASH && mv ./!($HASH) ./$HASH
+curl -L https://raw.githubusercontent.com/matteobrusa/Password-protection-for-static-pages/master/index.html >index.html  
+
+echo SITE encrypted
