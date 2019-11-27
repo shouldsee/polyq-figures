9be90378d009ca56c26011213992f4b0ab37ea02
shouldsee <shouldsee.gem@gmail.com>
2019-11-27 18:18:31 +0000

    Update make_encrypt_site.sh

--- make_encrypt_site.sh
+++ make_encrypt_site.sh
@@ -1,7 +1,7 @@
 ### password is reviewpaw
 DIR=${1:-$PWD}
 HASH=a93194bed5709a448522d69b9a722c02d1e7e71f
-find $DIR -type l -delete 
+find $DIR -type l -delete  ### jekyll does not work with symlink
 mkdir -p $HASH && mv ./!($HASH) ./$HASH
 curl -L https://raw.githubusercontent.com/matteobrusa/Password-protection-for-static-pages/master/index.html >index.html  
 
