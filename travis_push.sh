4f658dce6a36c0e514b22ce5ae3c64bbc7619c16
shouldsee <shouldsee.gem@gmail.com>
2019-11-28 12:12:46 +0000

    Update travis_push.sh

--- travis_push.sh
+++ travis_push.sh
@@ -8,10 +8,12 @@ setup_git() {
 commit_website_files() {
   git checkout -b gh-pages
   find . | tee LOCAL_INDEX
-  git add --all  
+  cat ../.gitignore
+  git add ../ --all  
 #  git add . *.html
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
+
 
 upload_files() {
   git remote add origin-pages https://${GITHUB_TOKEN}@github.com/${GITHUB_PATH}.git > /dev/null 2>&1
