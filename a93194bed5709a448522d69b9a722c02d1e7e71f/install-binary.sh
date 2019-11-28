09195732e370fb438bca69393b8bffb583923497
Travis CI <travis@travis-ci.org>
2019-11-28 13:34:21 +0000

    Travis build: 61

---
+++ a93194bed5709a448522d69b9a722c02d1e7e71f/install-binary.sh
@@ -0,0 +1,5 @@
+
+DIR_BIN=/usr/local/bin
+URL=https://github.com/arq5x/bedtools2/releases/download/v2.28.0/bedtools
+wget -nc $URL
+chmod +x bedtools; ln -sft $DIR_BIN $PWD/bedtools
---
+++ a93194bed5709a448522d69b9a722c02d1e7e71f/src/make_boxplot.py
@@ -0,0 +1,38 @@
+# %%python2
+from rnaseq_figure import job as template
+import pymisca.ext as pyext
+from src.util import _get_output_file
+
+
+def fig__saveFile(fig, ofname, transparent = False, **kwargs):
+    fig.savefig(ofname,
+                bbox_inches='tight',
+                        transparent= transparent,
+                        facecolor=fig.get_facecolor(),
+                        **kwargs)
+    return 1
+# !mkdir -p results
+for FNAME in [
+    'src/BOXPLOT_1_OX-lines.tsv',
+    'src/BOXPLOT2_mutants.tsv',
+    'src/BOXPLOT3_Qlines.tsv',
+]:
+    job = template.copy()
+
+    @pyext.setItem(job,'visualise_datasets')
+    def _func(self,key,datasets_meta,rnaseq):
+        df  = pyext.readData(FNAME).dropna()
+        return rnaseq.reindex(columns=df.index)
+    #     return df
+
+    fig = job['visualise_boxplot'];
+    fig.set_figwidth(7)
+    fig.set_figheight(7)
+    fig.axes[0].set_title(FNAME)
+#     OFNAME  = 'OUTPUT/'+FNAME.replace('/','_')+'.svg'
+#     with pyext.getPathStack(['OUTPUT'],force=1):
+    OFNAME = "OUTPUT/" + FNAME.replace('/','_')+'.svg'
+    OFNAME = _get_output_file(OFNAME)
+    fig__saveFile(fig,OFNAME)
+#     fig;
+# job['visualise_datasets'].head()
